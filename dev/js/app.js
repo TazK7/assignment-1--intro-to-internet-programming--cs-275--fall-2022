caa1651149b1638aafb84dd597988dea9565121e
TazK7 <khantazz07@gmail.com>
2022-12-09 14:16:04 -0500

    Resubmitted Assignment 1

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,56 +1,57 @@
 window.onload = () => {
-    let output = document.getElementById(`output`);
-    let content = ``;
-    let counter = 1;
+    let select = document.getElementById(`matrix`); //Selecting div in HTML with ID of matrix
+    let input = parseInt(window.prompt(`To create your matrix, insert a number greater than or equal to one.`));
+    //While loop confirms the input can't be less than 1, can't be anything other then a Number and can't be empty.
+    while (input < 1||isNaN(input)||input === null)
+        input = parseInt(window.prompt(`Try again. (No numbers less then 1, and no words.)`));
+    //Creating the New Array based off user input
+    let myNewArray = new Array(input);
 
-
-let input =  window.prompt(`Insert a Number Here`);
-input = parseInt(input, 10);
-console.log(typeof input);
-if (input < 2) {
-    alert(`Cannot use any number less than 2. Retry.`)
-} else if (isNaN(input) === true) {
-    alert(`The input is invalid. Retry.`)
-}
-
-let newArray = new Array(input);
-console.log(`This is the size of your array: ${newArray.length}`)
-
-content = `<table>`;
-
-    for(let i = 0; i < newArray.length; i++) {
-        content += `<tr>`;
-
-        for(let j = 0; j < newArray.length; j++) {
-            content += `<td>${counter++}</td>`;
+    //Using a for loop to increment through the Array based off input
+    for(let x = 0; x < input; x++){
+        myNewArray[x] = new Array(input);
+        for(let y = 0; y < input; y++){
+            myNewArray[x][y] = x * input + y + 1;
         }
-
-        content += `</tr>`;
     }
 
-    content += `</table>`;
-
-    output.innerHTML = content;
-
-
-    content = `<table>`;
-
-    for(let i = 0; i < newArray.length; i++) {
-        content += `<tr>`;
-
-    for(let j = 0; j < newArray.length; j++) {
-        let flipCounter = 1 + j + (i * newArray.length);
-        if (!((newArray.length + ((newArray.length - 1) * i)) === flipCounter)) {
-            flipCounter = newArray.length * newArray.length - flipCounter;
-            flipCounter++;
+    //Using a for loop and if statements to flip the second matrix
+    for(let x = 0; x < input; x++){
+        for(let y = 0; y < input; y++){
+            if ((input + (x * (input - 1))) !== (y + 1 + x * input)) {
+                //If statement calculates the middle coordinate of the Matrix and flips it after calculating diagonal
+                if((y + x * input) < parseInt(input*input / 2)){
+                    let flipped = myNewArray[x][y]; //Storing the flipped matrix inside a new var called flipped
+                    myNewArray[x][y] = myNewArray[input - x - 1][input - y - 1];
+                    //Assigning the flipped matrix (besides diagonal) inside var flipped
+                    myNewArray[input - x - 1][input - y - 1] = flipped;
+                }
+            }
         }
-        content += `<td>${flipCounter}</td>`;
     }
-
-    content += `</tr>`;
-}
-
-content += `</table>`;
-
-flipped.innerHTML = content;
+    //Using a function to store the code, then declaring it at the end to print Matrices
+    let printArray = (matrices) => {
+        let content = ``; //Creating empty content (same as examples repo) to populate later
+        for (let x = 0; x < input; x++) {
+            content += `<tr>`; //Putting the table row element inside content
+            for (let y = 0; y < input; y++) {
+                if((input + ((input - 1) * x)) === (y + 1 + (x * input))) {
+                    content += `<td id="diagonal">${(x * input) + 1 + y}</td>`;
+                } //First if statement adds diagonal id(highlight) if it calcuates the td being on the diagonal
+                else if(matrices === `first`){
+                    content += `<td>${(x * input) + 1 + y}</td>`;
+                } //Else if statments print both matrices (flipped & unflipped)
+                else if(matrices === `second`){
+                    content += `<td>${myNewArray[x][y]}</td>`;
+                }
+            }
+            content += `</tr>`; //Closing table row element
+        }
+        /* Using innerHTML to display the content (populated in printArray function)
+        inside the table all within the div Select*/
+        select.innerHTML += `<table>${content}</table>`;
+    };
+    //Delcaring the printArray function to display the matrices
+    printArray(`first`);
+    printArray(`second`);
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,109 @@
 const { src, dest, series, watch } = require(`gulp`),
-    htmlCompressor = require(`gulp-htmlmin`),
-    cssCompressor = require(`gulp-clean-css`),
-    jsValidator = require(`gulp-eslint`),
-    babel = require(`gulp-babel`),
+    htmlValidator = require(`gulp-htmlmin`),
+    cssValidator = require(`gulp-clean-css`),
     jsCompressor = require(`gulp-uglify`),
+    jsLinter = require(`gulp-eslint`),
+    babel = require(`gulp-babel`),
     browserSync = require(`browser-sync`),
     reload = browserSync.reload;
 
-    let compressHTML = () => {
-        return src(`dev/html/index.html`)
-            .pipe(htmlCompressor({collapseWhitespace: true}))
-            .pipe(dest(`prod`));
-    };
-    
-    let compressCSS = () => {
-        return src(`dev/css/style.css`)
-            .pipe(cssCompressor({collapseWhitespace: true}))
-            .pipe(dest(`prod`));
-    };
-    
-    let compressJS = () => {
-        return src(`dev/js/app.js`)
-            .pipe(babel())
-            .pipe(jsCompressor())
-            .pipe(dest(`prod`));
-    };
-    
-    let validateJS = () => {
-        return src(`dev/js/app.js`)
-        .pipe(jsValidator())
-        .pipe(jsValidator.formatEach(`compact`, process.stderr));
-    };
-    
-    let transpileJSForDev = () => {
-        return src(`dev/js/app.js`)
-            .pipe(babel())
-            .pipe(dest(`temp/scripts`));
-    };
-    
-    let transpileJSForProd = () => {
-        return src(`dev/js/app.js`)
-            .pipe(babel())
-            .pipe(jsCompressor())
-            .pipe(dest(`prod/scripts`));
-    };
-    
-    let serve = () => {
-        browserSync({
-            notify: true,
-            reloadDelay: 50,
-            server: {
-                baseDir: [
-                    `dev`,
-                    `dev/html`
-                ]
-            }
-        });
-    
-        watch(`dev/html/*.html`).on(`change`, reload);
-        watch(`dev/css/*.css`).on(`change`, reload);
-        watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
-    
-    };
-    
-    
-    
-    exports.validateJS = validateJS;
-    exports.transpileJSForDev = transpileJSForDev;
-    exports.compressHTML = compressHTML;
-    exports.compressCSS = compressCSS;
-    exports.compressJS = compressJS;
-    exports.transpileJSForProd = transpileJSForProd;
-    exports.serve = serve;
-    exports.build = series(
-        compressJS,
-        transpileJSForProd,
-        compressHTML,
-        compressCSS,
-    );
+let browserChoice = `default`;
+
+let serve = () =>
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `dev`,
+                `dev/html`
+            ]
+        }
+    });
+
+async function chrome () {
+    browserChoice = `chrome`;
+}
+
+let validateHTML = () => {
+    return src([
+        `dev/html/*.html`,
+        `dev/html/**/*.html`])
+        .pipe(htmlValidator(undefined));
+};
+
+let validateCSS = () => {
+    return src([
+        `dev/css/*.css`,
+        `dev/css/**/*.css`])
+        .pipe(cssValidator(undefined));
+};
+
+let validateJS = () => {
+    return src([
+        `dev/js/*.js`,
+        `dev/js/**/*.js`])
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let compressHTML = () => {
+    return src([`dev/html/*.html`,`dev/html/**/*.html`])
+        .pipe(htmlValidator({collapseWhitespace: true}))
+        .pipe(dest(`prod/html`));
+};
+
+let compressCSS = () => {
+    return src([`dev/css/*.css`,`dev/css/**/*.css`])
+        .pipe(cssValidator({collapseWhitespace: true}))
+        .pipe(dest(`prod/css`));
+};
+
+let compressJS = () => {
+    return src(`dev/js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let transpileJSForDev = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let transpileJSForProd = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+watch(`dev/html/*.html`, validateHTML).on(`change`, reload);
+watch(`dev/css/*.css`, validateCSS).on(`change`, reload);
+watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
+
+exports.validateHTML = validateHTML;
+exports.validateCSS = validateCSS;
+exports.validateJS = validateJS;
+exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
+exports.compressJS = compressJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
+exports.chrome = series(serve, chrome);
+exports.serve = series(
+    validateHTML,
+    validateCSS,
+    validateJS,
+    transpileJSForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    compressCSS,
+    compressJS,
+    transpileJSForProd
+);
